2877dae91a632aa30f91313db40c06a1604c7497
Kirtan Patel <kirtan251608@gmail.com>
2025-02-06 11:05:09 +0530

    initial frontend setup done

---
+++ frontend/src/components/HomeNav.jsx
@@ -0,0 +1,7 @@
+const HomeNav = () => {
+   return (
+      <h1 className="text-3xl">this is homenav</h1>
+   )
+}
+
+export default HomeNav
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import HomeNav from "../components/HomeNav"
+
+const HomePage = () => {
+   return (
+      <>
+      <div className="p-2">
+         <HomeNav />
+         <h1 className="text-3xl">this is home page</h1>
+      </div>
+      </>
+   )
+}
+
+export default HomePage
